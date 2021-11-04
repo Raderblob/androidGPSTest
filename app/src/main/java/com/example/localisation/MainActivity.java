46ce45f07c29ddd9040f243a436f790f97ab0327
Richard Marchant <39084971+Raderblob@users.noreply.github.com>
2021-11-04 11:15:54 +0100

    delay

--- app/src/main/java/com/example/localisation/MainActivity.java
+++ app/src/main/java/com/example/localisation/MainActivity.java
@@ -6,13 +6,21 @@ import android.Manifest;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
     Button btnShowLocation,btnShowLocation2;
+    TextView outputText;
+    EditText delayInput;
+    int delayTime;
 
     private static final String[] INITIAL_PERMS={
             Manifest.permission.ACCESS_FINE_LOCATION,
@@ -34,57 +42,87 @@ public class MainActivity extends AppCompatActivity {
 
         btnShowLocation = (Button) findViewById(R.id.btnShowLocation);
         btnShowLocation2 = (Button) findViewById(R.id.btnShowLocation2);
+        outputText = (TextView) findViewById(R.id.textView);
+        delayInput = (EditText) findViewById(R.id.editTextNumber);
 
         // Show location button click event
         btnShowLocation.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
-                // Create class object
-                gps = new GPSTracker(MainActivity.this, false);
+                Runnable r = new Runnable() {
+                    @Override
+                    public void run() {
+                        // Create class object
+                        gps = new GPSTracker(MainActivity.this, false);
 
-                // Check if GPS enabled
-                if(gps.canGetLocation()) {
+                        // Check if GPS enabled
+                        if(gps.canGetLocation()) {
 
-                    double latitude = gps.getLatitude();
-                    double longitude = gps.getLongitude();
+                            double latitude = gps.getLatitude();
+                            double longitude = gps.getLongitude();
 
-                    // \n is for new line
-                    String isMock = "Mocked: " + isMockSettingsON(MainActivity.this);
-                    Toast.makeText(getApplicationContext(), "Your gps Location is - \nLat: " + latitude + "\nLong: " + longitude+ "\n" + isMock, Toast.LENGTH_LONG).show();
-                } else {
-                    // Can't get location.
-                    // GPS or network is not enabled.
-                    // Ask user to enable GPS/network in settings.
-                    gps.showSettingsAlert();
-                }
+                            // \n is for new line
+                            String isMock = "Mocked: " + isMockSettingsON(MainActivity.this);
+                            outputText.setText( "Your gps Location is - \nLat: " + latitude + "\nLong: " + longitude+ "\n" + isMock);
+                        } else {
+                            // Can't get location.
+                            // GPS or network is not enabled.
+                            // Ask user to enable GPS/network in settings.
+                            gps.showSettingsAlert();
+                        }
+                    }
+                };
+
+                setDelay(r);
+
             }
         });
         btnShowLocation2.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
+                Runnable r = new Runnable() {
+                    @Override
+                    public void run() {
+                        gps = new GPSTracker(MainActivity.this, true);
+
+                        // Check if GPS enabled
+                        if(gps.canGetLocation()) {
+
+                            double latitude = gps.getLatitude();
+                            double longitude = gps.getLongitude();
+
+                            // \n is for new line
+                            String isMock = "Mocked: " + isMockSettingsON(MainActivity.this);
+                            outputText.setText("Your network Location is - \nLat: " + latitude + "\nLong: " + longitude + "\n" + isMock);
+                        } else {
+                            // Can't get location.
+                            // GPS or network is not enabled.
+                            // Ask user to enable GPS/network in settings.
+                            gps.showSettingsAlert();
+                        }
+                    }
+                };
+                setDelay(r);
                 // Create class object
-                gps = new GPSTracker(MainActivity.this, true);
 
-                // Check if GPS enabled
-                if(gps.canGetLocation()) {
-
-                    double latitude = gps.getLatitude();
-                    double longitude = gps.getLongitude();
-
-                    // \n is for new line
-                    String isMock = "Mocked: " + isMockSettingsON(MainActivity.this);
-                    Toast.makeText(getApplicationContext(), "Your network Location is - \nLat: " + latitude + "\nLong: " + longitude + "\n" + isMock , Toast.LENGTH_LONG).show();
-                } else {
-                    // Can't get location.
-                    // GPS or network is not enabled.
-                    // Ask user to enable GPS/network in settings.
-                    gps.showSettingsAlert();
-                }
             }
         });
     }
+
+    private void setDelay(Runnable r){
+        try {
+            delayTime = Integer.parseInt(delayInput.getText().toString());
+        }catch(Exception ex){
+            delayTime = 0;
+        }
+        outputText.setText("Waiting for " + delayTime + " seconds");
+
+        Handler handler = new Handler();
+        handler.postDelayed(r, delayTime *1000);
+    }
+
     public static boolean isMockSettingsON(Context context) {
         // returns true if mock location enabled, false if not enabled.
         if (Settings.Secure.getString(context.getContentResolver(),
